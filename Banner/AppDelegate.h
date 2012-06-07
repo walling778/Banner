08fe65c95d4af4d6243817f44ea302749d4ef106
William Brady Alling <walling@vinces-imac.dhcp.asu.edu>
2012-06-07 14:43:10 -0700

    Initial Commit

---
+++ Banner/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Banner
+//
+//  Created by William Brady Alling on 6/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Banner/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Banner
+//
+//  Created by William Brady Alling on 6/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
